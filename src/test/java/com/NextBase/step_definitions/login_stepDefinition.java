8a830f713a1095d9ebf01968c80f49a09d3217ec
SueTheCoder <suheyla315@hotmail.com>
2021-07-12 14:59:59 -0400

    step definitions

---
+++ src/test/java/com/NextBase/step_definitions/login_stepDefinition.java
@@ -0,0 +1,56 @@
+package com.NextBase.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class login_stepDefinition {
+
+    @Given("User is on the login page")
+    public void user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters hr username")
+    public void user_enters_hr_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters hr password")
+    public void user_enters_hr_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters helpdesk username")
+    public void user_enters_helpdesk_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters helpdesk password")
+    public void user_enters_helpdesk_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters marketing username")
+    public void user_enters_marketing_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters marketing password")
+    public void user_enters_marketing_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
